fa6893fd130744fc4121973dc55c99e49ddef990
IvanKorenevskiy <designbu-ivko@mail.ru>
2022-10-07 11:29:23 +0300

    Get all WooCommerce customers paid orders with a SQL query

--- wp-content/themes/twentytwentytwo/orders.php
+++ wp-content/themes/twentytwentytwo/orders.php
@@ -33,6 +33,25 @@ wp_reset_postdata();
 
 endif;
 ?>
+<?php
+function get_orders_ids_by_product_id( $product_id, $order_status = array( 'wc-completed' ) ){
+    global $wpdb;
+
+    $results = $wpdb->get_col("
+        SELECT order_items.order_id
+        FROM {$wpdb->prefix}woocommerce_order_items as order_items
+        LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta ON order_items.order_item_id = order_item_meta.order_item_id
+        LEFT JOIN {$wpdb->posts} AS posts ON order_items.order_id = posts.ID
+        WHERE posts.post_type = 'shop_order'
+        AND posts.post_status IN ( '" . implode( "','", $order_status ) . "' )
+        AND order_items.order_item_type = 'line_item'
+        AND order_item_meta.meta_key = '_product_id'
+        AND order_item_meta.meta_value = '$product_id'
+    ");
+
+    return $results;
+}
+?>
 
 <?php
 get_footer();
